96794e8ba6e8e6c7ba4c0448881282a86e754c57
igorfelyx031-oss <igor.felyx031@gmail.com>
2026-08-30 02:26:25 -0300

    Update MainActivity.java

--- app/src/main/java/com/copiloto/rotas/MainActivity.java
+++ app/src/main/java/com/copiloto/rotas/MainActivity.java
@@ -3,7 +3,10 @@ package com.copiloto.rotas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
@@ -11,12 +14,37 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView texto = new TextView(this);
-        texto.setText("Copiloto de Rotas");
-        texto.setTextSize(24);
-        texto.setTextColor(Color.WHITE);
-        texto.setPadding(40, 80, 40, 40);
+        FrameLayout tela = new FrameLayout(this);
+        tela.setBackgroundColor(Color.rgb(11, 18, 32));
 
-        setContentView(texto);
+        TextView botao = new TextView(this);
+
+        botao.setText("🟠\nCOPILOTO");
+        botao.setTextColor(Color.WHITE);
+        botao.setTextSize(14);
+        botao.setGravity(Gravity.CENTER);
+        botao.setPadding(18, 12, 18, 12);
+
+        GradientDrawable fundo = new GradientDrawable();
+        fundo.setColor(Color.rgb(234, 88, 12));
+        fundo.setCornerRadius(100);
+        botao.setBackground(fundo);
+
+        FrameLayout.LayoutParams tamanho =
+                new FrameLayout.LayoutParams(
+                        130,
+                        70
+                );
+
+        tamanho.gravity = Gravity.TOP | Gravity.END;
+        tamanho.setMargins(0, 60, 15, 0);
+
+        tela.addView(botao, tamanho);
+
+        botao.setOnClickListener(v -> {
+            botao.setText("🟢\nATIVO");
+        });
+
+        setContentView(tela);
     }
 }
